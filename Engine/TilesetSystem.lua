3ba6dfdbd680bd8b26b6d1b2c815cb727f57be01
Rami Sabbagh <ramilego4game@gmail.com>
2016-06-17 15:53:13 +0300

    Now can build a Tileset from a directory

--- Engine/TilesetSystem.lua
+++ Engine/TilesetSystem.lua
@@ -31,14 +31,34 @@ function TSS:buildTileset(path,name)
   local TiCH = love.thread.getChannel("TilesTSJ_"..name)
   local TinCH = love.thread.getChannel("TilesNamesTSJ_"..name)
   local tsList = JSON:decode(love.filesystem.read(path))
+  if tsList.Auto then tsList.Tiles = self:indexTiles(tsList.Auto) end
   for k, tn in ipairs(tsList.Tiles) do
-    TiCH:push(_Images[name.."_"..tn]:getData())
+    if tsList.Auto then TiCH:push(_Images[tn]:getData()) else TiCH:push(_Images[name.."_"..tn]:getData()) end
     TinCH:push(tn)
   end
   TiCH:push(false)
   TCH:push(name)
   TCH:push(tsList.TileSize)
   if not self.thread:isRunning() then self.thread:start() end
+end
+
+function TSS:indexTiles(dir)
+  local tiles = {}
+  local files = love.filesystem.getDirectoryItems(dir)
+  for k,filename in ipairs(files) do
+    if love.filesystem.isDirectory(dir..filename) then
+      local nT = self:indexTiles(dir..filename.."/")
+      for k, n in ipairs(nT) do
+        table.insert(tiles,n)
+      end
+    else
+      local p, n, e = splitFilePath(dir..filename)
+      if e == "png" then
+        table.insert(tiles,n)
+      end
+    end
+  end
+  return tiles
 end
 
 function TSS:update()
