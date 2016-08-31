18ff9f8866824f12a9239a66453cb40103818ff3
Rami Sabbagh <ramilego4game@gmail.com>
2016-08-31 20:50:49 +0300

    Tiled Compiler GUI !!!

--- Engine/DebugTools.lua
+++ Engine/DebugTools.lua
@@ -5,7 +5,11 @@ local FPSSize = 1000
 local FPS = {}
 for i=1, FPSSize do FPS[i] = 0 end
 
+local TCompiler = require("Engine.TiledCompiler")
+local CurrentTCState = "Select A map to\nshow info"
 local ShowTCWindow = false
+local SelectedTCFile = 0
+local TCFiles = {}
 
 local ShowLogsWindow = false
 local ShowFPSOverlay = false
@@ -19,6 +23,36 @@ function DT:updateFPS(fps)
       FPS[k] = FPS[k+1]
     end
   end
+end
+
+function DT:splitFilePath(path)
+  return path:match("(.-)([^\\/]-%.?([^%.\\/]*))$")
+end
+
+function DT:updateTCFilesList()
+  SelectedTCFile = 0 TCFiles = {}
+  local files = love.filesystem.getDirectoryItems("/Maps/Tiled/")
+  for k,filename in ipairs(files) do
+    local p, n, e = DT:splitFilePath("/Maps/Tiled/"..filename)
+    --n = n:sub(0,-5)
+    if e == "lua" then
+      --PMug.Shape[n] = require(string.gsub(path..n,"/","%."))
+      table.insert(TCFiles,n)
+    end
+  end
+end
+
+function DT:gatherTMapInfo(path)
+  local code = love.filesystem.load(path)
+  setfenv(code,{}) --Sandbox the code
+  local mapData = code()
+  local info = "-Version: "..mapData.version.."\n-LuaVersion: "..mapData.luaversion.."\n-Orientation:\n "..mapData.orientation..
+  "\n-W: "..mapData.width.." H: "..mapData.height.."\n-TW: "..mapData.tilewidth.." TH: "..mapData.tileheight..
+  "\n-Total Layers: "..#mapData.layers.."\n-Tilesets: "
+  for k,v in ipairs(mapData.tilesets) do
+    info = info.."\n "..v.name
+  end
+  return info
 end
 
 function DT:createMenus()
@@ -50,18 +84,25 @@ end
 
 function DT:insertMenuGames()
   imgui.MenuItem("Load")
+  if imgui.IsItemHovered() then imgui.SetTooltip("Loads a compiled level") end
+  if imgui.MenuItem("Exit") then love.event.quit() end
+  if imgui.IsItemHovered() then imgui.SetTooltip("Closes the game") end
 end
 
 function DT:insertMenuTools()
   if _Loaded then
     if imgui.MenuItem("Tiled Compiler",false,ShowTCWindow) then ShowTCWindow = not ShowTCWindow end
+    if imgui.IsItemHovered() then imgui.SetTooltip("Compiles Tiled Maps To Game's Format") end
   end
 end
 
 function DT:insertMenuDebugs()
   if imgui.MenuItem("Logs",false,ShowLogsWindow) then ShowLogsWindow = not ShowLogsWindow end
+  if imgui.IsItemHovered() then imgui.SetTooltip("The game engine logs.") end
   if imgui.MenuItem("FPS",false,ShowFPSOverlay) then ShowFPSOverlay = not ShowFPSOverlay end
+  if imgui.IsItemHovered() then imgui.SetTooltip("Shows A FPS Graph") end
   if imgui.MenuItem("Imgui Demo",false,ShowImguiDemoWindow) then ShowImguiDemoWindow = not ShowImguiDemoWindow end
+  if imgui.IsItemHovered() then imgui.SetTooltip("The demo window of ImGUI") end
 end
 
 function DT:createWindows()
@@ -71,7 +112,29 @@ end
 
 function DT:createToolsWindows()
   if ShowTCWindow then
-    imgui.Begin("Tiled Compiler")
+    imgui.SetNextWindowPos(25,45, "FirstUseEver")
+    imgui.SetNextWindowSize(430, 240, "FirstUseEver")
+    imgui.Begin("Tiled Compiler",_,{"NoResize","NoSavedSettings"})
+    local LBChanged
+    LBChanged, SelectedTCFile = imgui.ListBox(CurrentTCState.."###TCFilesList",SelectedTCFile,TCFiles,#TCFiles,10)
+    if LBChanged then
+      CurrentTCState = DT:gatherTMapInfo("/Maps/Tiled/"..TCFiles[SelectedTCFile])
+    end
+
+    imgui.Separator()
+    if imgui.Button("Refresh") then
+      self:updateTCFilesList()
+      CurrentTCState = "Refeshed Maps List"
+    end
+    if imgui.IsItemHovered() then imgui.SetTooltip("Refreshes the list of the maps") end
+
+    imgui.SameLine()
+    if imgui.Button("Compile") then
+      if SelectedTCFile == 0 then CurrentTCState = "Please select a map\nfirst to compile" return end
+      TCompiler:compileTiled("/Maps/Tiled/"..TCFiles[SelectedTCFile],"/TiledBuilds/"..TCFiles[SelectedTCFile])
+      CurrentTCState = "Compiled Map:\n "..TCFiles[SelectedTCFile].."\n To: /TiledBuilds/"
+    end
+    if imgui.IsItemHovered() then imgui.SetTooltip("Compiles the selected map") end
 
     imgui.End()
   end
@@ -93,5 +156,7 @@ function DT:createDebugWindows()
     imgui.End()
   end
 end
+
+DT:updateTCFilesList()
 
 return DT
