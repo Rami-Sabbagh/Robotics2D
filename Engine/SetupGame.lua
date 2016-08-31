b7994fc91d4e22e75fc2c585b0464110b50ce120
Rami Sabbagh <ramilego4game@gmail.com>
2016-08-31 20:50:33 +0300

    Added new folder setup

--- Engine/SetupGame.lua
+++ Engine/SetupGame.lua
@@ -1,13 +1,13 @@
 --This file is part of PixelizerBox--
 --[[
   * Copyright 2015 RamiLego4Game
-  * 
+  *
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
   * You may obtain a copy of the License at
-  * 
+  *
   *     http://www.apache.org/licenses/LICENSE-2.0
-  * 
+  *
   * Unless required by applicable law or agreed to in writing, software
   * distributed under the License is distributed on an "AS IS" BASIS,
   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,32 +37,33 @@ end
 
 function SetupGame:init()
   _PixelFont = Font("Libs/Fonts/Pixel.ttf")
-  
+
   _Loaded = false
 
   _Width, _Height = love.graphics.getDimensions()
   _SystemOS = love.system.getOS()
-  
+
   if _SystemOS == "Android" or (arg and arg[2] == "-android") then _Android = true end
-  
+
   love.graphics.setDefaultFilter("nearest","nearest")
-  
+
   _Tileset = {}
-  
+
   --Loader Variables--
   _Images,_Sounds,_Fonts,_ImageDatas,_rawJData,_rawINIData,_ImageQualities,_Maps,_TEXTUREQUALITY = {},{},{},{},{},{},{},{},"High"
-  
+
   --_Cursor = love.mouse.newCursor(love.graphics.newImage("/Libs/Misc/SSHCursor.png"):getData())
   --love.mouse.setCursor(_Cursor)
-  
+
   _Light = false
-  
+
   if _Light then
     require("Helpers.LightVsShadow.light")
     require("Helpers.LightVsShadow.postshader")
   end
-  
-  if not love.filesystem.exists("/TSB") then love.filesystem.createDirectory("/TSB") end
+
+  if not love.filesystem.exists("/TSB") then love.filesystem.createDirectory("/TSB") end --Tilesets Builds
+  if not love.filesystem.exists("/TiledBuilds") then love.filesystem.createDirectory("/TiledBuilds") end
   if not love.filesystem.exists("/Screenshots") then love.filesystem.createDirectory("/Screenshots") end
 end
 
