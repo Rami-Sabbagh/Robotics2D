efbe337fd1b51785f6a0000a22f34a838574d679
Rami Sabbagh <ramilego4game@gmail.com>
2016-09-03 06:22:57 +0300

    Documented the code + map format

--- Engine/TiledCompiler.lua
+++ Engine/TiledCompiler.lua
@@ -1,6 +1,28 @@
 --[[
-  Tiled Compiler is an Engine tool for robotics2d to compile Tiled maps to the game map form, 
+  Tiled Compiler is an Engine tool for robotics2d to compile Tiled maps to the game map form,
   It's going to be used temp just soon as the Level Editor get programmed.
+
+  The format of robotics2d map: [shortcut: rm]
+  rm.w = The width of the map in tiles.
+  rm.h = The height of the map in tiles.
+  rm.t = The set of tiles used (Key is id, Value is tile name).
+  rm.d = The map data.
+
+  The format of rm data:
+  The map size = rm.w*rm.h
+  * Every layer take the double size of the map size (Because it's constructed of data & rotation data)
+  The layer size = The map size * 2
+  The number of the layers in the map: #rm.d/The layer size
+  * The layer is constructed of data & rotation data
+  The first index of data = LayerID*The LayerSize
+  The first index of rotation data = The first index of data + The map size
+  * The data contains tileids
+  tilename = rm.t[data value]
+  * The rotation data holds (filpX,flipY,rotate 90,rotate -90) 4 options, so I stored them in a nibble (half byte)
+  if bit.band(rotData,1)==1 then FlipX is true end
+  if bit.band(rotData,2)==2 then FlipY is true  end
+  if bit.band(rotData,4)==4 then rotate 90 is true end
+  if bit.band(rotData,8)==8 then rotate -90 is true end
 
   Rotation bits:
   1:flipX
@@ -14,16 +36,17 @@ local LuaTable = require("Engine.LuaTable")
 local TC = {}
 
 function TC:compileTiled(path,targetpath,dump)
-  local tm = love.filesystem.load(path)()
-  local rm, tilesets, tiles = {}, {}, {}
-  rm.w, rm.h, rm.t, rm.d = tm.width, tm.height, {}, {}
+  local tm = love.filesystem.load(path)() --TiledMap
+  local rm, tilesets, tiles = {}, {}, {} --RM: Robotics2DMap.
+  rm.w, rm.h, rm.t, rm.d = tm.width, tm.height, {}, {} --W: Width, H: Height, T: Tiles (Used Tiles), D: Data
 
-  rm.ts = tm.tilewidth
+  rm.ts = tm.tilewidth --TS: tilesize
 
   for k,v in ipairs(tm.tilesets) do
-    table.insert(tilesets,{name=v.name,firstgid=v.firstgid -1})
+    table.insert(tilesets,{name=v.name,firstgid=v.firstgid -1}) --Copy the tileset, tilesets have name, firstgid.
   end
 
+  --Returns the tileset for the the given id + The id of the tile within the tileset.
   local function getTileset(gid)
     for k,v in ipairs(tilesets) do
       if gid <= v.firstgid then
@@ -33,38 +56,39 @@ function TC:compileTiled(path,targetpath,dump)
     return tilesets[#tilesets].name, gid - tilesets[#tilesets].firstgid
   end
 
-  for k,layer in ipairs(tm.layers) do
-    if layer.type == "tilelayer" and layer.visible then
-      local rotData = {}
-      for k,gid in ipairs(layer.data) do
-        if gid == 0 then
-          table.insert(rm.d,0)
+  for k,layer in ipairs(tm.layers) do --Loop throw the layers of the tiled map.
+    if layer.type == "tilelayer" and layer.visible then --Filter to accept only visible tile layers.
+      local rotData = {} --Rotation Data
+      for k,gid in ipairs(layer.data) do --Loop throw the gids of the tiles in the layar data.
+        if gid == 0 then --0 means no tile.
+          table.insert(rm.d,0) --Store the tiles data in seperate tables, gid, rotdata.
           table.insert(rotData,0)
         else
-          local rgid, rd, tsname = self:getGID(gid)
-          local tsname, ngid = getTileset(rgid)
-          local tname = _Tileset[tsname].GIDs[ngid]
-          if not tiles[tname] then
-            table.insert(rm.t,tname)
-            tiles[tname] = #rm.t
+          local rgid, rd, tsname = self:getGID(gid) --rgid: Real gid, rd: Rotation Data, tsname: TilesetName
+          local tsname, ngid = getTileset(rgid) --ngid: New gid.
+          local tname = _Tileset[tsname].GIDs[ngid] --Get the tile name from the gid.
+          if not tiles[tname] then --If the tile is not added before
+            table.insert(rm.t,tname) --Add it to the map.
+            tiles[tname] = #rm.t --And add it to a local table for making writing maps easier.
           end
-          table.insert(rm.d,tiles[tname])
-          table.insert(rotData,rd)
+          table.insert(rm.d,tiles[tname]) --Add the tile id to the data array.
+          table.insert(rotData,rd) --Add the rotation data of the tile to the rotation data array.
         end
       end
       for k, rd in ipairs(rotData) do
-        table.insert(rm.d,rd)
+        table.insert(rm.d,rd) --Now merge the rotation data with the data array.
       end
     end
   end
 
+  --If saving path specified, encode the map using LuaTable
   if targetpath then
     local code = LuaTable.encode(rm)
     if dump then code = string.dump(code) end
     love.filesystem.write(targetpath,code)
   end
 
-  return rm
+  return rm --Also return the loaded map array.
 end
 
 function TC:getGID(gid) --Taken from STI.map.setFlippedGID
